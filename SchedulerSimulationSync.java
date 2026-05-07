64dd65056487cf4eeed8455c3d3d258f16048105
sarah9510is <bbbnn7789@gmail.com>
2026-05-07 23:17:03 +0300

    protect the shared variable completedProcessCount using ReentrantLock lock and in finally release the lock to prevent deadlock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -59,7 +59,13 @@ class SharedResources {
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+      lock. lock();
+try {
+contextSwitchCount++;
+}finally {
+lock. unlock ();
+}
+ 
     }
     
     // Method to add waiting time
